b2f346e7accffc9e1d4ff356de5c4fca23c31448
rummolprod999 <rummolprod999@gmail.com>
2019-03-19 15:42:21 +0300

    update

--- Sources/Freelance_bot/FilesOperations.swift
+++ Sources/Freelance_bot/FilesOperations.swift
@@ -6,12 +6,15 @@ import Foundation
 
 class FilesOperations {
     static func deleteLogFile(_ logFile: String) {
+        let fm = FileManager.default
+        if !fm.fileExists(atPath: logFile){
+            return
+        }
         do {
             let attr = try FileManager.default.attributesOfItem(atPath: logFile)
             let fileSize = attr[FileAttributeKey.size] as! UInt64
             if fileSize > 1_000_000 {
-                let fileManager = FileManager.default
-                try fileManager.removeItem(atPath: logFile)
+                try fm.removeItem(atPath: logFile)
             }
         } catch {
             log.error("Failed get log file size or delete this file")
--- Sources/Freelance_bot/main.swift
+++ Sources/Freelance_bot/main.swift
@@ -8,11 +8,11 @@ file.format = "$DHH:mm:ss.SSS$d $L $N.$F:$l - $M"
 log.addDestination(file)
 
 func main() {
+    FilesOperations.deleteLogFile(logFile)
     log.info("start parser")
     let b = Bot(url: "https://freelance.ru/projects/?spec=4")
     b.run()
     log.info("end parser")
-    FilesOperations.deleteLogFile(logFile)
 }
 
 main()
